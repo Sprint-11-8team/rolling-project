7928a8826c2b5034be2169ce3607467530766d32
yunrap <dbsdpwl7@gmail.com>
2024-11-20 00:40:09 +0900

    SCSS 적용해보기

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
+import ListPage from "../pages/ListPage";
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/list" element={<ListPage />} />
       </Routes>
     </BrowserRouter>
   );
