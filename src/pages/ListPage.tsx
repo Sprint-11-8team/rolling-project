9773390cbea471782946ac5fa1e6a9cc784210ad
yunrap <dbsdpwl7@gmail.com>
2024-11-26 17:10:43 +0900

    카드컴포넌트 스와이퍼 적용

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,10 +1,35 @@
+import { Swiper, SwiperSlide } from "swiper/react";
 import CardList from "../components/cardList/Card";
 
 const ListPage = () => {
   return (
     <div className="list-page-container">
       <div className="label">인기 롤링 페이퍼</div>
-      <CardList />
+      <Swiper
+        className="mySwiper"
+        pagination={true}
+        pagination-type="progressbar"
+        navigation={true}
+      >
+        <SwiperSlide>
+          <CardList />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardList />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardList />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardList />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardList />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardList />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
