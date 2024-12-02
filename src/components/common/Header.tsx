c76fba3df95bdd933c34d018a1d80c60d99b8793
unknown <smm45108@gmail.com>
2024-12-02 20:43:59 +0900

    fix : 헤더의  롤링페이기만들기 버튼 메인페이지에서만 보이게 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,14 +1,19 @@
 import icon from "../../assets/icons/homeIcon.svg";
+import { useLocation } from "react-router-dom";
 const Header = () => {
+  const location = useLocation();
+  console.log(location);
   return (
     <div className="header">
       <div className="header-nav">
         <a href="/">
           <img className="header-icon" src={icon}></img>
         </a>
-        <button className="header-rolling-button">
-          <div className="rolling-paper-text">롤링 페이퍼 만들기</div>
-        </button>
+        {location.pathname === "/" && ( // 경로가 '/' 일때만 렌더링
+          <button className="header-rolling-button">
+            <div className="rolling-paper-text">롤링 페이퍼 만들기</div>
+          </button>
+        )}
       </div>
     </div>
   );
