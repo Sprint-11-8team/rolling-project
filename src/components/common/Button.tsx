f2b4f6afac21cb607eb875cf4ddf94b0c178db10
yunrap <dbsdpwl7@gmail.com>
2024-12-04 20:36:47 +0900

    [목록 ] 레이아웃 수정, 라우터 이동 및 공통컴포넌트 주석 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,6 +1,19 @@
-import { ReactNode } from 'react';
-import classes from '../../utils/classes';
+import { ReactNode } from "react";
+import classes from "../../utils/classes";
 
+/*
+ * <pre>
+ * @title Button.tsx
+ * @desc Button 공통컴포넌트
+ * </pre>
+ *
+ * @author 김진희
+ * @since 2024.11.24
+ * @version 0.1.0
+ * @see =================== 변경 내역 ==================
+ *   날짜       변경자     내용
+ *  2024.11.24.  김진희  최초작성
+ */
 interface ButtonProps {
   children?: ReactNode;
   addClassName?: string | string[];
@@ -8,10 +21,20 @@ interface ButtonProps {
   disabled: boolean;
 }
 
-function Button({ children, addClassName, handleClick, disabled }: ButtonProps) {
+function Button({
+  children,
+  addClassName,
+  handleClick,
+  disabled,
+}: ButtonProps) {
   return (
     <>
-      <button type="button" className={classes('common-button', addClassName)} onClick={handleClick} disabled={disabled}>
+      <button
+        type="button"
+        className={classes("common-button", addClassName)}
+        onClick={handleClick}
+        disabled={disabled}
+      >
         {children}
       </button>
     </>
